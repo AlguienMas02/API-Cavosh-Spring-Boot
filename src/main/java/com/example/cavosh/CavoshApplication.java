9124d56b8dca7a5904d4ec1e5ae121fb21a23f1c
AlguienMas020 <mpaulinodurand2015@gmail.com>
2025-11-09 22:47:07 -0500

    inicio

---
+++ src/main/java/com/example/cavosh/CavoshApplication.java
@@ -0,0 +1,13 @@
+package com.example.cavosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CavoshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CavoshApplication.class, args);
+	}
+
+}
